8c5205606fb390e59276b97a5fc08195dd4305f0
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-26 19:16:41 +0200

    [UPD] searchbar end

--- src/pages/DetailTrip.jsx
+++ src/pages/DetailTrip.jsx
@@ -1,45 +1,65 @@
 import trips from "../data/db";
 import { useState, useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import ParticipantCard from "../components/ParticipantCard";
 
 const DetailTrip = () => {
   const { id } = useParams();
-  const [currentTrip, setCurrentTrip] = useState({});
+  const [currentTrip, setCurrentTrip] = useState(null);
+  const [filteredParticipants, setFilteredParticipants] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
-    setCurrentTrip(trips.find((trip) => trip.id == id));
+    // trova il trip
+    const trip = trips.find((trip) => trip.id == id);
+    setCurrentTrip(trip);
+
+    // inizializza i partecipanti
+    if (trip) {
+      setFilteredParticipants(trip.participants);
+    }
   }, [id]);
 
-  // searchbar
+  useEffect(() => {
+    if (currentTrip) {
+      const tempParticipants = currentTrip.participants.filter((p) =>
+        `${p.name} ${p.surname}`.toLowerCase().includes(search.toLowerCase())
+      );
+      setFilteredParticipants(tempParticipants);
+    }
+  }, [search, currentTrip]);
+
+  if (!currentTrip) {
+    return <p>Loading...</p>;
+  }
 
   return (
-    <div className="container d-flex flex-column align-items-center mt-4">
-      <div>
-        <div className="container-fluid">
-          <div className="row">
-            {/* Search */}
-            <div className="col-12">
-              <form className="d-flex" role="search">
-                <input
-                  className="form-control me-2"
-                  type="search"
-                  placeholder="Cerca partecipante"
-                  aria-label="Cerca partecipante"
-                />
-                <button className="btn btn-outline-success" type="submit">
-                  Cerca
-                </button>
-              </form>
-            </div>
-          </div>
-          {/* Participant */}
-          <div className="col-12 mt-3">
-            <h1>{currentTrip && currentTrip.name}</h1>
-            <h3>Partecipanti</h3>
-            <ParticipantCard currentTrip={currentTrip} id={id} />
-          </div>
+    <div className="container-fluid">
+      <div className="row">
+        {/* Search */}
+        <div className="col-12">
+          <form
+            className="d-flex"
+            role="search"
+            onSubmit={(e) => e.preventDefault()}
+          >
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Cerca"
+              aria-label="Cerca"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </form>
         </div>
+      </div>
+
+      {/* Participant */}
+      <div className="col-12">
+        <h1>{currentTrip.name}</h1>
+        <h2>Partecipanti</h2>
+        <ParticipantCard participants={filteredParticipants} />
       </div>
     </div>
   );
