86a893befe6e02c331d4252b602fbc7d1c1a518f
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-26 11:00:50 +0200

    [UPD] BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import DefaultLayout from "./layouts/DefaultLayout";
 import DetailTrip from "./pages/DetailTrip";
 import HomePage from "./pages/HomePage";
-import { Router, Routes, Route } from "react-router-dom";
+import { Router, Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Router>
+      <BrowserRouter>
         <Routes>
           <Route element={<DefaultLayout />}>
             <Route path="/" element={<HomePage />} />
             <Route path="/trips/:id" element={<DetailTrip />} />
           </Route>
         </Routes>
-      </Router>
+      </BrowserRouter>
     </>
   );
 }
