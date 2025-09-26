7d0d0a024f420eadbdd299b990acddf4e6472c89
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-26 19:09:22 +0200

    [UPD] searchbar funzionante

--- src/components/ParticipantCard.jsx
+++ src/components/ParticipantCard.jsx
@@ -1,20 +1,16 @@
-import trips from "../data/db";
-import { Link } from "react-router-dom";
-
-const ParticipantCard = ({ currentTrip, id }) => {
+const ParticipantCard = ({ participants }) => {
   return (
     <div>
-      {currentTrip.participants &&
-        currentTrip.participants.map((participant) => (
-          <div key={participant.participantId}>
-            <p>
-              {participant.name} {participant.surname}
-            </p>
-            <p>{participant.phone}</p>
-            <p>{participant.email}</p>
-            <p>{participant.fiscalCode}</p>
-          </div>
-        ))}
+      {participants.map((participant) => (
+        <div key={participant.participantId}>
+          <p>
+            {participant.name} {participant.surname}
+          </p>
+          <p>{participant.phone}</p>
+          <p>{participant.email}</p>
+          <p>{participant.fiscalCode}</p>
+        </div>
+      ))}
     </div>
   );
 };
--- src/pages/DetailTrip.jsx
+++ src/pages/DetailTrip.jsx
@@ -1,44 +1,65 @@
 import trips from "../data/db";
 import { useState, useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import ParticipantCard from "../components/ParticipantCard";
 
 const DetailTrip = () => {
   const { id } = useParams();
-  const [currentTrip, setCurrentTrip] = useState({});
+  const [currentTrip, setCurrentTrip] = useState(null);
+  // creo filteredParticipants per far si che l'array non si azzerri dopo la ricerca
+  const [filteredParticipants, setFilteredParticipants] = useState([]);
+  // variabile di stato per Search
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
-    setCurrentTrip(trips.find((trip) => trip.id == id));
+    // trova il trip
+    const trip = trips.find((trip) => trip.id == id);
+    setCurrentTrip(trip);
+
+    // inizializza i partecipanti
+    if (trip) {
+      setFilteredParticipants(trip.participants);
+    }
   }, [id]);
 
-  // searchbar
-
+  useEffect(() => {
+    if (currentTrip) {
+      const tempParticipants = currentTrip.participants.filter((p) =>
+        `${p.name} ${p.surname}`.toLowerCase().includes(search.toLowerCase())
+      );
+      setFilteredParticipants(tempParticipants);
+    }
+  }, [search, currentTrip]);
 
   return (
-    <div>
-      <div className="container-fluid">
-        <div className="row">
-          {/* Search */}
-          <div className="col-12">
-            <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Cerca"
-                aria-label="Cerca"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Cerca
-              </button>
-            </form>
-          </div>
-        </div>
-        {/* Participant */}
+    <div className="container-fluid">
+      <div className="row">
+        {/* Search */}
         <div className="col-12">
-          <h1>{currentTrip && currentTrip.name}</h1>
-          <h2>Partecipanti</h2>
-          <ParticipantCard currentTrip={currentTrip} id={id} />
+          <form
+            className="d-flex"
+            role="search"
+            onSubmit={(e) => e.preventDefault()}
+          >
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Cerca"
+              aria-label="Cerca"
+              // value search
+              value={search}
+              // permette il cambio di visualizzazione dopo la search
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </form>
         </div>
+      </div>
+
+      {/* Participant */}
+      <div className="col-12">
+        <h1>{currentTrip.name}</h1>
+        <h2>Partecipanti</h2>
+        <ParticipantCard participants={filteredParticipants} />
       </div>
     </div>
   );
