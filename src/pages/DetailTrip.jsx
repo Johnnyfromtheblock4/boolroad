747ba3d448bc13d0cd36eec57eb5299b7512d7a1
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-26 13:08:01 +0200

    [UPD] map inizio

--- src/pages/DetailTrip.jsx
+++ src/pages/DetailTrip.jsx
@@ -16,7 +16,7 @@ const DetailTrip = () => {
 
   // useEffect
   useEffect(fetchCurrentTrip, []);
-  console.log(currentTrip);
+  // console.log(currentTrip);
 
   return (
     <div>
@@ -40,62 +40,40 @@ const DetailTrip = () => {
       </div>
       {/* Lista Partecipanti */}
       <div className="col-12">
-        <h1>{trips[0].name}</h1>
+        <h1>{currentTrip.name}</h1>
         <h2>Partecipanti</h2>
-        <div className="accordion" id="accordionExample">
-          <div className="accordion-item">
-            <h2 className="accordion-header">
-              <button
-                className="accordion-button"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseOne"
-                aria-expanded="true"
-                aria-controls="collapseOne"
-              >
-                {trips[0].participants[0].name}
-                {trips[0].participants[0].surname}
-              </button>
-            </h2>
-            <div
-              id="collapseOne"
-              className="accordion-collapse collapse show"
-              data-bs-parent="#accordionExample"
-            >
-              <div className="accordion-body">
-                <p>{trips[0].participants[0].phone}</p>
-                <p>{trips[0].participants[0].email}</p>
-                <p>{trips[0].participants[0].fiscalCode}</p>
+        {currentTrip.participants.map((participant) => {
+          return (
+            <div className="accordion" id="accordionExample">
+              <div className="accordion-item">
+                <h2 className="accordion-header">
+                  <button
+                    className="accordion-button"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseOne"
+                    aria-expanded="true"
+                    aria-controls="collapseOne"
+                  >
+                    {trips[0].participants[0].name}
+                    {trips[0].participants[0].surname}
+                  </button>
+                </h2>
+                <div
+                  id="collapseOne"
+                  className="accordion-collapse collapse show"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div className="accordion-body">
+                    <p>{trips[0].participants[0].phone}</p>
+                    <p>{trips[0].participants[0].email}</p>
+                    <p>{trips[0].participants[0].fiscalCode}</p>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="accordion-item">
-            <h2 className="accordion-header">
-              <button
-                className="accordion-button collapsed"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseTwo"
-                aria-expanded="false"
-                aria-controls="collapseTwo"
-              >
-                {trips[0].participants[1].name}
-                {trips[0].participants[1].surname}
-              </button>
-            </h2>
-            <div
-              id="collapseTwo"
-              className="accordion-collapse collapse show"
-              data-bs-parent="#accordionExample"
-            >
-              <div className="accordion-body">
-                <p>{trips[0].participants[1].phone}</p>
-                <p>{trips[0].participants[1].email}</p>
-                <p>{trips[0].participants[1].fiscalCode}</p>
-              </div>
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
