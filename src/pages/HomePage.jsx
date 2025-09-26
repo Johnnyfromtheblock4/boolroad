d3dde8a84bd5dee651dfec4f20aaeb3194042044
amletodellaloggia <amletodellaloggia@gmail.com>
2025-09-26 23:48:35 +0200

    [UPD] installed/imported: fontawesome, HomePage.jsx (style, added img section to cards) [ADD] HomePage.css, images folder

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,8 @@
 import trips from "../data/db";
 import { Link } from "react-router-dom";
+import "../styles/HomePage.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 const HomePage = () => {
   return (
@@ -8,15 +11,29 @@ const HomePage = () => {
       <div className="row justify-content-center">
         {trips.map((trip) => (
           <div key={trip.id} className="col-md-6 col-lg-4 mb-4">
-            <div className="card h-100 text-center">
+            <div className="card h-100 text-center slate-card">
+              <div className="card-img slate-img">
+                <img
+                  src={`/images/${trip.image}`}
+                  alt={trip.name}
+                  className="slate-img-photo"
+                />
+              </div>
               <div className="card-body">
                 <h5 className="card-title">
-                  <Link to={`/trips/${trip.id}`} className="text-decoration-none">
+                  <Link
+                    to={`/trips/${trip.id}`}
+                    className="text-decoration-none"
+                  >
                     {trip.name}
                   </Link>
                 </h5>
-                <p className="card-text"> {trip.city}</p>
-                <p className="card-text"> {trip.initialDate} - {trip.endDate}</p>
+                <p className="card-text">
+                  {trip.city} <FontAwesomeIcon icon={faMapLocationDot} />
+                </p>
+                <p className="card-text">
+                  {trip.initialDate} - {trip.endDate}
+                </p>
               </div>
             </div>
           </div>
