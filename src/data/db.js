e1f035ea9f5e1121ee18f205b5c30cb080344b01
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-26 10:27:00 +0200

    [UPD] db.js

---
+++ src/data/db.js
@@ -0,0 +1,25 @@
+const trips = [
+  {
+    id: 1,
+    name: "Gita al lago",
+    city: "Como",
+    initialDate: "2025-11-14",
+    endDate: "2025-11-17",
+    participants: [
+      {
+        name: "Amleto",
+        surname: "Mure",
+        fiscalCode: "VHSDVD10F15G400Z",
+        email: "amletomure@proton.ch",
+        phone: 3338585072,
+      },
+      {
+        name: "Gianmarco",
+        surname: "Visconti",
+        fiscalCode: "PS2PSP44V24L777W",
+        email: "gianvisco@gmail.it",
+        phone: 34518635741,
+      },
+    ],
+  },
+];
