ff69d7c6cb2a7296303a7e58e2200945e8e74552
anthony-murè <tony.mure99@gmail.com>
2025-09-26 19:44:36 +0200

    [MDF]style-HomePage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header>
-      <nav className="navbar bg-body-tertiary">
+      <nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
         <div className="container-fluid">
           <Link to="/" className="navbar-brand">
             BoolRoad
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,22 +3,24 @@ import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div>
-      <div className="container-fluid">
-        <h1>Lista Viaggi</h1>
-        <div className="row">
-          <div className="col-12">
-            {trips.map((trip) => (
-              <div key={trip.id} className="trip-card">
-                <Link to={`/trips/${trip.id}`}>{trip.name}</Link>
-                <p>{trip.city}</p>
-                <p>
-                  {trip.initialDate} → {trip.endDate}
-                </p>
+    <div className="container mt-5">
+      <h1 className="text-center mb-4">Lista Viaggi</h1>
+      <div className="row justify-content-center">
+        {trips.map((trip) => (
+          <div key={trip.id} className="col-md-6 col-lg-4 mb-4">
+            <div className="card h-100 text-center">
+              <div className="card-body">
+                <h5 className="card-title">
+                  <Link to={`/trips/${trip.id}`} className="text-decoration-none">
+                    {trip.name}
+                  </Link>
+                </h5>
+                <p className="card-text"> {trip.city}</p>
+                <p className="card-text"> {trip.initialDate} - {trip.endDate}</p>
               </div>
-            ))}
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
