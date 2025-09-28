0d1afc564193864455e1d48d77678895942aad4f
amletodellaloggia <amletodellaloggia@gmail.com>
2025-09-28 12:22:15 +0200

    [UPD] general style and CSS, Header.jsx (added app icon) [ADD] Header.css (separated header css), added app icon to index

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import "../styles/Header.css";
 
 const Header = () => {
   return (
     <header>
       <nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
-        <div className="container-fluid">
+        <div className="container-fluid justify-content-start">
+          <img className="boolroad-logo" src="/boolroad.png" alt="BoolRoad" />
           <Link to="/" className="navbar-brand">
             <span className="first-part-title">.bool</span>Road
           </Link>
