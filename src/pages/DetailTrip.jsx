4d25ca9822daa03fb7d3e3b5425d1d13916477db
amletodellaloggia <amletodellaloggia@gmail.com>
2025-09-28 14:34:03 +0200

    [UPD] Style (final-commit)

--- src/pages/DetailTrip.jsx
+++ src/pages/DetailTrip.jsx
@@ -7,6 +7,7 @@ import "../styles/DetailTrip.css";
 import { faUsers } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faImage } from "@fortawesome/free-solid-svg-icons";
+import { faArrowsTurnRight } from "@fortawesome/free-solid-svg-icons";
 
 const DetailTrip = () => {
   const { id } = useParams();
@@ -62,7 +63,7 @@ const DetailTrip = () => {
 
       {/* Participant */}
       <div className="col-12">
-        <h1 className="my-3"><FontAwesomeIcon icon={faImage} />{currentTrip.name}</h1>
+        <h2 className="my-3"><FontAwesomeIcon icon={faArrowsTurnRight} />  {currentTrip.name}  </h2>
         <h3>
           <FontAwesomeIcon icon={faUsers} /> Partecipanti
         </h3>
