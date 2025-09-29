f4d83a265f2902717f2ae4f0e69025f3ea7bf696
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-29 11:35:01 +0200

    [UPD] booRoad

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,8 +7,8 @@ const Header = () => {
       <nav className="navbar bg-dark border-bottom border-body p-0" data-bs-theme="dark">
         <div className="container-fluid justify-content-start">
           <Link to="/" className="navbar-brand">
-          <img className="boolroad-logo" src="/boolroad.png" alt="BoolRoad" />
-            <span className="first-part-title">.bool</span><span>Road</span>
+          <img className="booroad-logo" src="/booroad.png" alt="BooRoad" />
+            <span className="first-part-title">.boo</span><span>Road</span>
           </Link>
         </div>
       </nav>
